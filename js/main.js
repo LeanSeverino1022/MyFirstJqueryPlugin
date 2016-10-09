d4c1cd7fbb3c311ce9daddd0f7c3cb9bda2febd8
Leandro Severino <yihseverino@gmail.com>
2016-10-09 17:15:13 +0800

    +stickyJS plugin
    main.js / sticky.js / work.html:
    plugin implemenation and minor code cleanup
    
    main.css
    - header bg applied
    - header bg changes color when it becomes a sticky element... applied
    transition effect too

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+  $(document).ready(function() {
+  
   $(".animsition").animsition({
     inClass: 'fade-in-left-lg',
-    outClass: 'fade-out-right-lg',  
+    outClass: 'fade-out-right-lg', 
+    linkElement: ".animsition-link", 
     inDuration: 500,
     outDuration: 500,  
+  });
+
+  $(".header").sticky();
+
   });
