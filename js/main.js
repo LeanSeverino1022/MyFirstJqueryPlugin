68055bf13edb233c3bbea19aa843e379889d8ef7
Leandro Severino <yihseverino@gmail.com>
2016-10-11 06:41:46 +0800

    new sticky element  and tested plugin's functionalities
    main,js
    - added a new sticky element $(".grid-full h5")
    - made use of stickyJS' "getWidthFrom" and "responsiveWidth" property.
    
    main.css, work.html
    minor styling / content changes

--- js/main.js
+++ js/main.js
@@ -8,18 +8,33 @@
     outDuration: 500,  
   });
 
-  $(".header").sticky();
+  $(".header").sticky({
+    responsiveWidth: true,
+    getWidthFrom: ".container"
+  });
+
+  $(".grid-full h5").sticky({ topSpacing: 60  });
 
     //sample use of plugin events
     $('.header').on('sticky-start', function() {
       //check work.html h1 element with class 'description'
-       $("h1.description").html("Changed");    
+       $("h1.description").html("Changed");  
+         $(".header").css({"height" : "60px" });
       });
 
   $('.header').on('sticky-end', function() {
-      $("h1.description").html("bla bla is what we do");  
-      console.log("abaw");  
+      $("h1.description").html("bla is what we do");  
+      console.log("abaw");
+
     });
+
+  $('.grid-full h5').on('sticky-start', function () {
+    $(this).append('<a href="mailto:someone@yoursite.com" target="_top" class="email-text"> Send Mail</a>');
+  });
+
+  $('.grid-full h5').on('sticky-end', function() {
+      $(".email-text").remove();
+  });
 
 
   });
